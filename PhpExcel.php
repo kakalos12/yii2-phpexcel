1289f03989a760e23e0d9de53bd9d720646964f6
Dong Nguyen <kakalos12@gmail.com>
2014-02-02 10:17:53 +0700

    get instance

--- PhpExcel.php
+++ PhpExcel.php
@@ -1,6 +1,18 @@
 <?php
+
+namespace kakalos12\phpexcel;
+
 use yii\base\Component;
 
 class PhpExcel extends Component {
+
+	private $_phpexcel = null;
+
+	public function createInstance() {
+		if (! isset ( $this->_phpexcel )) {
+			$this->_phpexcel = new \PHPExcel ();
+		}
+		return $this->_phpexcel ;
+	}
 
 }
